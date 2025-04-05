b979f7dcdececc92083b3639747ce4ce9b17e621
Darien <Darien@DESKTOP-SP6VBHD>
2025-04-04 21:17:58 -0400

    nuget push test

---
+++ RestaurantReviewCoreMVC/Models/AccountModel.cs
@@ -0,0 +1,37 @@
+﻿namespace RestaurantReviewCoreMVC.Models
+{
+    public class AccountModel
+    {
+        private int accountID;
+        private string name;
+        private string email;
+        private string password;
+        private string accountType;
+
+        public int AccountID
+        {
+            get { return accountID; }
+            set { accountID = value; }
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+        public string AccountType
+        {
+            get { return accountType; }
+            set { accountType = value; }
+        }
+    }
+}
