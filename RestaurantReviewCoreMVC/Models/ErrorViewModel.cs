678a1d2d5467f67563569bbe9ec53501d13923e9
Darien <Darien@DESKTOP-SP6VBHD>
2025-04-04 20:33:11 -0400

    Add project files.

---
+++ RestaurantReviewCoreMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RestaurantReviewCoreMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
