ec34fe0154c78772f60ad3d25045fec6213e2f68
jeffyfurious <tywertman17@gmail.com>
2025-04-11 23:00:32 -0400

    Addiing Files, planning infrastructure

---
+++ RestaurantReviewCoreMVC/Controllers/ProfileController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RestaurantReviewCoreMVC.Controllers
+{
+    public class ProfileController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
